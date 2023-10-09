c57d7c3b83b3fa3084a56db72a7f27803bca1b1e
Walter Bivanco <walterbivanco@gmail.com>
2023-10-09 09:27:56 -0300

    Se termino con docker y postgres

--- prueba/settings.py
+++ prueba/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'prueba.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "db_django_docker",
+        "USER": "postgres",
+        "PASSWORD": "",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
 

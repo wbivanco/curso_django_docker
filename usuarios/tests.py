30e082c0156c5480b2c107f5674c13fbbbd3a8f3
Walter Bivanco <walterbivanco@gmail.com>
2023-10-09 11:00:36 -0300

    Se termino con la seccion pruebas unitarias

--- usuarios/tests.py
+++ usuarios/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+
+class CustomUserTests(TestCase):
+    def test_crear_usuario(self):
+        Usu = get_user_model()
+        usu = Usu.objects.create_user(
+            username = "walter",
+            email = "walter@mail.com",
+            password = "123"
+        )
+
+        # Que esperamos
+        self.assertEqual(usu.username, "walter")
+        self.assertEqual(usu.email, "walter@mail.com")
+        self.assertTrue(usu.is_active)
+        self.assertFalse(usu.is_staff)
+        self.assertFalse(usu.is_superuser)
+
+    def test_crear_superusuario(self):
+        Usu = get_user_model()
+        usu = Usu.objects.create_superuser(
+            username = "walter",
+            email = "walter@mail.com",
+            password = "123"
+        )
+
+        # Que esperamos
+        self.assertEqual(usu.username, "walter")
+        self.assertEqual(usu.email, "walter@mail.com")
+        self.assertTrue(usu.is_active)
+        self.assertTrue(usu.is_staff)
+        self.assertTrue(usu.is_superuser)

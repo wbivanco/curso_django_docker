00e8f6a6c39a8a711d1aba5e77656063cf62a26b
Walter Bivanco <walterbivanco@gmail.com>
2023-10-10 09:36:17 -0300

    Hecho los templates

--- paginas/tests.py
+++ paginas/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class AboutPageTests(SimpleTestCase):
+    def setUp(self):
+        self.response = self.client.get("/pages/about")
+
+    def test_url_about(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_html_about(self):
+        self.assertTemplateUsed(self.response, "about.html")
+        self.assertContains(self.response, "Desarrollo de App de Combustibles")
+        self.assertNotContains(self.response, "Walter B")
+
+    # def test_existe_ruta(self):
+    #     response = self.client.get("/pages/about")
+    #     self.assertEqual(response.status_code, 200)
+
+    # def test_nombre_ruta_about(self):
+    #     response = self.client.get(reverse("pages:about"))
+    #     self.assertEqual(response.status_code, 200)
+
+    # def test_plantilla_contiene_html(self):
+    #     response = self.client.get(reverse("pages:about"))
+    #     self.assertContains(response, "Desarrollo de App de Combustibles")
+
+    # def test_plantilla_no_contiene_html(self):
+    #     response = self.client.get(reverse("pages:about"))
+    #     self.assertNotContains(response, "Walter B")
